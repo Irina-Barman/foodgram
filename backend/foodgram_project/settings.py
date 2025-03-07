92c508ab7ed81cbe12cf85fcb7a16a34ad615347
Ирина Барман <attame@yandex.ru>
2025-03-07 12:20:03 +0500

    подготовка к выгрузке

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="token")
 DEBUG = bool(os.getenv("DEBUG", "False").lower())
 
 ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS", default="127.0.0.1, localhost"
+    "ALLOWED_HOSTS", default="127.0.0.1, localhost",
 ).split(",")
 
 
@@ -67,9 +67,13 @@ WSGI_APPLICATION = "foodgram_project.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
