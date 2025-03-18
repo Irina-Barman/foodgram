c684cf8278f487e00e2f0833142ab2c849ff7999
Ирина Барман <attame@yandex.ru>
2025-03-18 12:54:27 +0500

    теги 2.0

--- infra/init.sql
+++ infra/init.sql
@@ -8,3 +8,14 @@ COPY ingredients(name, measurement_unit)
 FROM '/docker-entrypoint-initdb.d/ingredients.csv'
 DELIMITER ','
 CSV HEADER;
+
+
+CREATE TABLE IF NOT EXISTS tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL UNIQUE
+);
+
+COPY tags(name, slug)
+FROM '/docker-entrypoint-initdb.d/tags.csv'
+DELIMITER ','
+CSV HEADER;
