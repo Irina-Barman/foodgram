066083f021af4612a8103f4c1996805ad3f40b2d
Ирина Барман <attame@yandex.ru>
2025-03-20 12:04:28 +0500

    ред фильтр

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,31 +12,29 @@ User = get_user_model()
 
 
 class RecipeFilter(FilterSet):
-    """"Фильтр для сортировки рецептов.""" ""
-    queryset = Recipe.objects.prefetch_related('tags', 'ingredients').all()
-    author = ModelChoiceFilter(queryset=User.objects.all())
+    """Фильтр для сортировки рецептов."""
+    author = ModelChoiceFilter(queryset=User .objects.all())
     tags = django_filters.CharFilter(method="filter_tags")
     is_favorited = BooleanFilter(method="filter_is_favorited")
     is_in_shopping_cart = BooleanFilter(method="filter_is_in_shopping_cart")
 
     class Meta:
         model = Recipe
-        fields = ["author", "tags"]
+        fields = ["author", "tags", "is_favorited", "is_in_shopping_cart"]
 
     def filter_tags(self, queryset, name, value):
-        tag_slugs = self.request.GET.getlist("tags")
-        return queryset.filter(tags__slug__in=tag_slugs).distinct()
+        if not value:
+            return queryset
+        return queryset.filter(tags__slug__in=value).distinct()
 
     def filter_is_favorited(self, queryset, name, value):
-        if self.request.user.is_authenticated:
-            if value:
-                return queryset.filter(in_favorites__user=self.request.user)
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(in_favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if self.request.user.is_authenticated:
-            if value:
-                return queryset.filter(shopping_cart__user=self.request.user)
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
 
