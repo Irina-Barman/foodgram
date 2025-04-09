b5f5de0b2eb7963ade355c2accbe8ef79a8e31f5
Ирина Барман <attame@yandex.ru>
2025-04-09 16:08:47 +0500

    отключаем list и retrieve

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     re_path(r"^auth/", include("djoser.urls.authtoken")),
     path(
         "recipes/<int:id>/favorite/",
-        FavoritesViewSet,
+        FavoritesViewSet.as_view(),
         name="favorite",
     ),
     path(
--- backend/api/views.py
+++ backend/api/views.py
@@ -265,6 +265,18 @@ class FavoritesViewSet(ModelViewSet):
         favorite.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    def list(self, request, *args, **kwargs):
+        return Response(
+            {"detail": "Метод не разрешен."},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response(
+            {"detail": "Метод не разрешен."},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
 
 class TagViewSet(ReadOnlyModelViewSet):
     """Вьюсет для модели тега."""
