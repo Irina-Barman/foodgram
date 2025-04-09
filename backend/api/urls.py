8cc7b793fbd29ac88101cf7b78ece437f4449f32
Ирина Барман <attame@yandex.ru>
2025-04-09 19:42:43 +0500

    ред роутер

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,7 +11,6 @@ router_v1.register("tags", TagViewSet)
 router_v1.register("users", CustomUserViewSet)
 router_v1.register("ingredients", IngredientViewSet)
 router_v1.register("recipes", RecipeViewSet)
-router_v1.register("shopping_cart", ShoppingCartViewSet)
 
 urlpatterns = [
     path("", include(router_v1.urls)),
