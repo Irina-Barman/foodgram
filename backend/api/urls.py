5304f363f133c12e38c0386b35277d442ed39c7d
Ирина Барман <attame@yandex.ru>
2025-04-12 17:37:11 +0500

    лишний импорт

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     CustomUserViewSet,
-    FavoritesViewSet,
     IngredientViewSet,
     RecipeViewSet,
     ShoppingCartViewSet,
